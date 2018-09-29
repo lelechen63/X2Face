352eb2a8436e54c54461585bab2bb61a5d78cc38
Lele Chen <lchen63@j17.cs.rochester.edu>
2018-09-29 17:14:02 -0400

    gpu version to cpu

--- UnwrapMosaic/audio2face.py
+++ UnwrapMosaic/audio2face.py
@@ -105,6 +105,7 @@ for sourcepath in sourcepaths:
         cc += 1
         imsave('/mnt/ssd0/project/lchen63/X2Face/UnwrapMosaic/results/%d.jpg'%cc,gg )
         print (ggt.shape)
+        print (type(ggt))
         ggt = ggt.transpose((1,2, 0))
         print (result.size())
         imsave('/mnt/ssd0/project/lchen63/X2Face/UnwrapMosaic/results/gt_%d.jpg'%cc,ggt )
