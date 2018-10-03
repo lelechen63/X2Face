362c65b18d7ebbdeb4df02b978faf555d3e2bf7c
Lele Chen <lchen63@j17.cs.rochester.edu>
2018-10-03 16:15:57 -0400

    g

--- UnwrapMosaic/audio2face2.py
+++ UnwrapMosaic/audio2face2.py
@@ -35,8 +35,8 @@ def mat2npy(path):
     # d = np.load('examples/audio_features/Cristin_Milioti/1.6/IblJpk1GDZA/0004575.npz')['audio_feat']
     # print (d)
     # print (d.shape)
-# mat2npy('/u/lchen63/data/audio/Eddie_Kaye_Thomas/Eddie_Kaye_Thomas_227.mat')
-
+mat2npy('/u/lchen63/data/audio/EzraMiller/EzraMiller_75.mat')
+#
 
 
 # sys.exit("Error message")
@@ -64,7 +64,7 @@ def load_img_and_audio1(file_path):
     return {'image' : img, 'audio' : audio_feature}
    # paths to source frames
 
-sourcepaths= [['/u/lchen63/Desktop/Eddie_Kaye_Thomas/Eddie_Kaye_Thomas_00227.jpg', '/u/lchen63/data/audio/Eddie_Kaye_Thomas/Eddie_Kaye_Thomas_227.npy']]
+sourcepaths= [['/u/lchen63/Desktop/EzraMiller/EzraMiller_00075.jpg', '/u/lchen63/data/audio/EzraMiller/EzraMiller_75.npy']]
 
 # path to frames corresponding to driving audio features
 audio_path = 'examples/audio_faces/Peter_Capaldi/1.6/uAgUjSqIj7U'
